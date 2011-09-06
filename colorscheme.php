a9342f1b5b489e0329a2bb904a1d4bb41b2f4084
Amy Groshek <amy.groshek@remote-learner.net>
2011-09-06 14:46:22 -0500

    new g-search + css, color scheme

---
+++ colorscheme.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+	<head>
+	<style>
+	div {
+	display: inline-block;
+}
+</style>
+</head>
+	<body>
+		<div style="background-color:#8CFECD;width:100px;height:100px;">8CFECD</div>
+		<div style="background-color:#6f9886;width:100px;height:100px;">6f9886</div>
+		<div style="background-color:#000;width:100px;height:100px;">000</div>
+		<!-- <div style="background-color:#0D196F;width:100px;height:100px;"></div> -->
+		<!-- <div style="background-color:#010731;width:100px;height:100px;"></div>
+		<div style="background-color:#1f2342;width:100px;height:100px;"></div> -->
+		<!-- <div style="background-color:#6a74bf;width:100px;height:100px;"></div> -->
+		<!-- <div style="background-color:#e67f0b;width:100px;height:100px;"></div> -->
+		<div style="background-color:#FA8602;width:100px;height:100px;">FA8602</div>
+		<div style="background-color:#FAB76B;width:100px;height:100px;">FAB76B</div>
+		<div style="background-color:#ccc;width:100px;height:100px;">ccc</div> 
+</body>
+</html>
--- scripts/rt-custom.js
+++ scripts/rt-custom.js
@@ -1,35 +1,35 @@
-/* 
-   Custom javascript functionality
-   Written by Amy Groshek
-   2011-06-28
- */
+//   rt-custom.js 
+//   Custom javascript functionality
+//   Written by Amy Groshek
+//   2011-06-28
+//
+//
+jQuery( function() {
+	//console.log('is ready');
 
-// Facebook icon click
-jQuery('#likeonfb .clickicon').click(function(e) {
-	//console.log('fb click');
-	window.open('http://www.facebook.com/raintaxi','','width=400,height=600');
-});
+	var changePr = {
+	 isPrompt: false, // Has prompt already been changed
+	 check: function() { 
+		// Check for presence of prompt, if exists, change val, if not, reset timer
+		//console.log('check');
+		// if exists from google, then we change the input
+		if ( jQuery('input.gsc-input').length >= 1 && changePr.isPrompt == false ){
+		 //console.log('exists');
+		 changePr.isPrompt = true;
+		 var searchPrompt = 'Search Rain Taxi';
+		 var valObj = jQuery('input.gsc-input').val( searchPrompt );
+		 var addClass = jQuery('input.gsc-search-button').addClass('button rtgradient');
+		} else {
+		 changePr.timer();
+		}
+	 },
+	 timer: function() {
+		setTimeout(changePr.check, 1000);
+	 }
+	};
+	changePr.timer();
 
-// Twitter icon click
-jQuery('#followontwitter .clickicon').click(function(e){
-	//console.log('twitter click');
-	window.open('http://www.twitter.com/RainTaxiReview','','width=400,height=600');
-});
+ }
 
-// Join mailing list click
-jQuery('#joinmailinglist .clickicon').click( function(e){
-	//console.log('mailing click');
-	window.open('http://raintaxi.com/contact.shtml','','width=400,height=600');
-});
 
-// Donate click
-jQuery('#donate .clickicon').click( function(){
-	//console.log('mailing click');
-	window.open(' http://www.raintaxi.com/support.shtml','','width=400,height=600');
-});
-
-jQuery('.searchform input').click( function(){
-	//console.log('input click');
-	jQuery(this).attr('value','');
-});
-
+);
